ac65975dc28695376f5aaf21544713ee44e637ea
Rakesh gombi <rakeshgombi18@gmail.com>
2021-06-27 17:31:34 +0000

    Updates

---
+++ precache-manifest.6d9fe6f67455b16826dd8a1053e9c475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206dbfc21fe61220380c465fdb40b783",
+    "url": "/CrypTracker/index.html"
+  },
+  {
+    "revision": "6a7f65182627cc75c8f6",
+    "url": "/CrypTracker/static/css/main.795e7574.chunk.css"
+  },
+  {
+    "revision": "0df63eee97bd6ee9ae60",
+    "url": "/CrypTracker/static/js/2.e1258fa5.chunk.js"
+  },
+  {
+    "revision": "6a7f65182627cc75c8f6",
+    "url": "/CrypTracker/static/js/main.e62cef3c.chunk.js"
+  },
+  {
+    "revision": "116a256fac47507f6778",
+    "url": "/CrypTracker/static/js/runtime-main.3b04de14.js"
+  }
+]);
